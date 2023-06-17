1f30455babfd2b2dba893eeb675fa4f4351dba30
Lutfi Esa Praestyo <out1051@fifgroup.co.id>
2023-06-17 20:35:19 +0700

    First Commit

---
+++ Scripts/Test Android/Script1687005484799.groovy
@@ -0,0 +1,50 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.configuration.RunConfiguration as RunConfiguration
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import io.appium.java_client.AppiumDriver
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.WebElement
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.mobile.helper.MobileElementCommonHelper
+import com.kms.katalon.core.mobile.keyword.internal.MobileDriverFactory
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.KeywordUtil
+import io.appium.java_client.AppiumDriver
+
+def path = RunConfiguration.getProjectDir() + '/Data Files/selendroid-test-app.apk'
+
+Mobile.startApplication(path, false)
+
+CustomKeywords.'keyword.test.android.utilities.popup'()
+
+TestData datainput = findTestData('Data Files/Test Data (1)')
+
+'Start Process'
+WS.comment('Start Process')
+	
+String inputdata1 = datainput.getValue("inputdata1", 1)
+String inputdata2 = datainput.getValue("inputdata2", 1)
+
+CustomKeywords.'keyword.test.android.utilities.testAndroid'(inputdata1, inputdata2)
+
+'End Process'
+WS.comment('End Process')
